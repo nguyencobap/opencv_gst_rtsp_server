74e695823636641080e09a86eb64e017ea6981f6
vutung1671997 <vutung1671997@gmail.com>
2023-08-01 22:49:15 +0700

    NguyenNH: thêm bật tắt debug log

--- opencv_gst_rtsp_server/utils/log_utils.py
+++ opencv_gst_rtsp_server/utils/log_utils.py
@@ -1,7 +1,13 @@
 import logging
+import os
+
+is_debug: bool = False
+if 'OPENCV_GST_RTSP_SERVER_DEBUG' in os.environ:
+    is_debug = os.environ['OPENCV_GST_RTSP_SERVER_DEBUG'].lower() == "true"
 
 logger: logging.Logger = logging.getLogger('logger')
-logger.setLevel(logging.DEBUG)
+if is_debug:
+    logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname).1s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s')
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
