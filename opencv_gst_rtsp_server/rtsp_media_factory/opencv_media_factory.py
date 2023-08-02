dc1f929cc4ad9716ad2a45976122012bd4b47cdd
vutung1671997 <vutung1671997@gmail.com>
2023-08-02 23:50:17 +0700

    NguyenNH: pipeline node convention

--- opencv_gst_rtsp_server/rtsp_media_factory/opencv_media_factory.py
+++ opencv_gst_rtsp_server/rtsp_media_factory/opencv_media_factory.py
@@ -15,9 +15,9 @@ class OpenCVMediaFactory(GstRtspServer.RTSPMediaFactory):
     pipeline: Gst.Element = None 
     use_gpu: bool = False
     use_h265: bool = False
-    videoconvert: str = '! videoconvert ! video/x-raw,format=I420 '
-    encoder: str = '! x264enc '
-    rtppay: str = '! rtph264pay config-interval=1 name=pay0 pt=96 '
+    videoconvert: str = 'videoconvert ! video/x-raw,format=I420'
+    encoder: str = 'x264enc'
+    rtppay: str = 'rtph264pay config-interval=1 name=pay0 pt=96'
 
     def __init__(self, **properties):
         super(OpenCVMediaFactory, self).__init__(**properties)
@@ -32,19 +32,19 @@ class OpenCVMediaFactory(GstRtspServer.RTSPMediaFactory):
             self.pipeline.set_state(Gst.State.NULL)
 
     def do_create_element(self, url: GstRtsp.RTSPUrl):
-        self.rtppay = '! rtph265pay config-interval=1 name=pay0 pt=102 ' if self.use_h265 else '! rtph264pay config-interval=1 name=pay0 pt=96 '
-        self.videoconvert: str = '! nvvideoconvert ! video/x-raw(memory:NVMM),format=I420 ' if self.use_gpu else '! videoconvert ! video/x-raw,format=I420 '
+        self.rtppay = 'rtph265pay config-interval=1 name=pay0 pt=102' if self.use_h265 else 'rtph264pay config-interval=1 name=pay0 pt=96'
+        self.videoconvert: str = 'nvvideoconvert ! video/x-raw(memory:NVMM),format=I420' if self.use_gpu else 'videoconvert ! video/x-raw,format=I420'
 
         if self.use_gpu:
-            self.encoder: str = '! nvv4l2h265enc ' if self.use_h265 else '! nvv4l2h264enc '
+            self.encoder: str = 'nvv4l2h265enc' if self.use_h265 else 'nvv4l2h264enc'
         else:
-            self.encoder: str = '! x265enc ' if self.use_h265 else '! x264enc '
+            self.encoder: str = 'x265enc' if self.use_h265 else 'x264enc'
 
         self.launch_string = 'appsrc name=source is-live=true block=true format=GST_FORMAT_TIME ' \
                     f'caps=video/x-raw,format=BGR,width={self.width},height={self.height},framerate={self.fps}/1 ' \
-                    f'{self.videoconvert}' \
-                    f'{self.encoder}' \
-                    f'{self.rtppay}'
+                    f'! {self.videoconvert} ' \
+                    f'! {self.encoder} ' \
+                    f'! {self.rtppay} '
 
         logger.debug(f"launch_string = {self.launch_string}")
         self.pipeline = Gst.parse_launch(self.launch_string)
