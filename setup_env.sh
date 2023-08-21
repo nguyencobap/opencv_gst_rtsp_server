9ce4edb3f3daa051a877c7388833463110cd9ffb
nguyencobap <nguyenhainguyen97@gmail.com>
2023-08-21 15:49:37 +0700

    NguyenNH: thêm package environment

--- setup_env.sh
+++ setup_env.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo apt install gir1.2-gst-rtsp-server-1.0 gstreamer1.0-plugins-ugly -y
+sudo apt install gir1.2-gst-rtsp-server-1.0 gstreamer1.0-plugins-ugly libgirepository1.0-dev libcairo2-dev python3.8-dev -y
